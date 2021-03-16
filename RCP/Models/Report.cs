590f1fa79fc90c997c634f56805649b74b0deecc
Kajetan Boruta <kajetan.boruta@bosmal.com.pl>
2021-03-16 12:58:38 +0100

    added class Report

---
+++ RCP/Models/Report.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RCP.Models
+{
+    public class Report
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int WorkerID { get; set; }
+        public Worker Worker { get; set; }
+        public int ClientID { get; set; }
+        public Client Client { get; set; }
+    }
+}
