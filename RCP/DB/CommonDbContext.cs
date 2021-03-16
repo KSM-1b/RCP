9337b30254129044e2d83cc21bf5e99a3af5e171
Kajetan Boruta <kajetan.boruta@bosmal.com.pl>
2021-03-16 12:07:20 +0100

    added database Context class

---
+++ RCP/DB/CommonDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RCP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RCP.DB
+{
+    public class CommonDbContext : DbContext
+    {
+        DbSet<Client> Clients { get; set; }
+        DbSet<Job> Jobs { get; set; }
+        DbSet<Worker> Workers { get; set; }
+
+        public CommonDbContext(DbContextOptions<CommonDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
