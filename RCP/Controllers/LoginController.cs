bbc743ebc3444258e7788e91def2fed14f93394c
seba300 <sebastiandomagala98@gmail.com>
2021-05-22 14:37:04 +0200

    Create Register and Login controller

---
+++ RCP/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RCP.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET
+        public string Index()
+        {
+            return "test";
+        }
+    }
+}
---
+++ RCP/Controllers/RegisterController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RCP.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET
+        public string Index()
+        {
+            return "test";
+        }
+    }
+}
