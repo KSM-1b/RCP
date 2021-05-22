87b93ac5ebcca416b8463f182c1a086d402c0e96
seba300 <sebastiandomagala98@gmail.com>
2021-05-22 15:12:32 +0200

    Create Register form

--- RCP/Controllers/HomeController.cs
+++ RCP/Controllers/HomeController.cs
@@ -34,5 +34,6 @@ namespace RCP.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
     }
 }
--- RCP/Models/User.cs
+++ RCP/Models/User.cs
@@ -9,5 +9,6 @@ namespace RCP.Models
         [Key]
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
     }
 }
