6e1ce6617cc449b6e78daa1e27e314a0b39cec1a
seba300 <sebastiandomagala98@gmail.com>
2021-05-22 13:32:31 +0200

    Added data annotation in User model

--- RCP/Models/User.cs
+++ RCP/Models/User.cs
@@ -1,8 +1,12 @@
 ﻿
+
+using System.ComponentModel.DataAnnotations;
+
 namespace RCP.Models
 {
     public class User
     {
+        [Key]
         public string Username { get; set; }
         public string Password { get; set; }
     }
