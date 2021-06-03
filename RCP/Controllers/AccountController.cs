36a4b7d146b640d98ec9b3b05b4835f66bdbd8ab
seba300 <sebastiandomagala98@gmail.com>
2021-06-03 21:25:10 +0200

    Create Login GET action

--- RCP/Controllers/AccountController.cs
+++ RCP/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using RCP.ViewModel;
@@ -52,6 +53,13 @@ namespace RCP.Controllers
                 
             }
             return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
         }
     }
 }
