1366c36aefe3c584029d3ab29e7a054c2a8f0cac
seba300 <sebastiandomagala98@gmail.com>
2021-06-07 21:25:27 +0200

    create AspNetUsers migration

--- RCP/Migrations/20210607192210_User to AspNetUsers conversion.Designer.cs
+++ RCP/Migrations/20210607192210_User to AspNetUsers conversion.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RCP.DB;
@@ -9,9 +10,10 @@ using RCP.DB;
 namespace RCP.Migrations
 {
     [DbContext(typeof(CommonDbContext))]
-    partial class CommonDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210607192210_User to AspNetUsers conversion")]
+    partial class UsertoAspNetUsersconversion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
