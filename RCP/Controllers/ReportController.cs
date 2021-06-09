6176c2b1c8ef56e55cc5170be40f8f54d7496b33
seba300 <sebastiandomagala98@gmail.com>
2021-06-09 20:11:09 +0200

    Create ReportController constructor

--- RCP/Controllers/ReportController.cs
+++ RCP/Controllers/ReportController.cs
@@ -1,10 +1,20 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RCP.DB;
 
 namespace RCP.Controllers
 {
     public class ReportController : Controller
     {
+        
+        private readonly CommonDbContext _context;
+
+        public ReportController(CommonDbContext context)
+        {
+            _context = context;
+        }
+        
         // GET
         public Task<IActionResult> Index()
         {
