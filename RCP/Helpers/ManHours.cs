855f1709a560a73015a9b739dd574126111d34aa
seba300 <sebastiandomagala98@gmail.com>
2021-06-16 22:34:06 +0200

    Create method to calculate spent hours

--- RCP/Helpers/ManHours.cs
+++ RCP/Helpers/ManHours.cs
@@ -6,7 +6,9 @@ namespace RCP.Helpers
     {
         public double GetManHours(DateTime startDate, DateTime endDate)
         {
-            
+            double minutes = endDate.Subtract(startDate).TotalMinutes;
+            double hours = minutes / 60;
+            return hours;
         }
     }
 }
