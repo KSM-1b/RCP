47f278d0f28d1a83a01c0b62f6701df9586c23c7
seba300 <sebastiandomagala98@gmail.com>
2021-06-16 23:15:16 +0200

    Create condition

--- RCP/Helpers/ManHours.cs
+++ RCP/Helpers/ManHours.cs
@@ -8,6 +8,13 @@ namespace RCP.Helpers
         {
             double minutes = endDate.Subtract(startDate).TotalMinutes;
             double hours = minutes / 60;
+            
+            //If in database someone inserted startDate bigger than endDate
+            if (hours < 0)
+            {
+                hours=0;
+            }
+            
             return hours;
         }
     }
