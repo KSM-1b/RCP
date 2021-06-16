b829d100af29801a5d4c25e93b1dceae9802f500
seba300 <sebastiandomagala98@gmail.com>
2021-06-16 23:14:41 +0200

    Create second unit test

--- RCP.Tests/ManHoursTests.cs
+++ RCP.Tests/ManHoursTests.cs
@@ -12,12 +12,27 @@ namespace RCP.Tests
             //Arrange
             ManHours manHours = new ManHours();
             DateTime startTask = new DateTime(2021, 2, 12, 10, 15, 0);
-            DateTime endTask = new DateTime(2021, 2, 12, 10, 45, 0);
+            DateTime endTask = new DateTime(2021, 2, 12, 11, 45, 0);
             
             //Act
             double result = manHours.GetManHours(startTask, endTask);
             
-            Assert.AreEqual(30,result);
+            //Assert
+            Assert.AreEqual(1.5,result);
+        }
+        [Test]
+        public void GetManHours_ReturnsProperValueWhenNegativeValue()
+        {
+            //Arrange
+            ManHours manHours = new ManHours();
+            DateTime startTask = new DateTime(2021, 2, 12, 10, 15, 0);
+            DateTime endTask = new DateTime(2021, 2, 12, 9, 45, 0);
+            
+            //Act
+            double result = manHours.GetManHours(startTask, endTask);
+            
+            //Assert
+            Assert.AreEqual(0,result);
         }
     }
 }
