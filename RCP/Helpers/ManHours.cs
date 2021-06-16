06ca5f6b634c468919cb5508ff6f97add8e61b8e
seba300 <sebastiandomagala98@gmail.com>
2021-06-16 22:29:46 +0200

    Create ManHours class to calculated time spent on this task

---
+++ RCP/Helpers/ManHours.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RCP.Helpers
+{
+    public class ManHours
+    {
+        public double GetManHours(DateTime startDate, DateTime endDate)
+        {
+            
+        }
+    }
+}
