ca49b9acd3b38c71043ca1f7fedd5cdfaa59ed70
kajetanboruta <kajetanboruta@gmail.com>
2021-06-26 15:30:15 +0200

    fix: controller changes

--- RCP/Controllers/ReportController.cs
+++ RCP/Controllers/ReportController.cs
@@ -23,40 +23,52 @@ namespace RCP.Controllers
             _context = context;
         }
 
-        // GET
-        public async Task<IActionResult> Index()
+        private async Task<List<Report>> GetReports()
         {
-            List<Report> reportList = _context.Reports
-                .Include(x=>x.Client)
-                .Include(y=>y.Worker)
-                .ToList();
+            List<Report> reportList = await _context.Reports
+                                        .Include(x => x.Client)
+                                        .Include(y => y.Worker)
+                                        .ToListAsync();
+            return reportList;
+        }
 
+        private async Task<List<ReportViewModel>> ConvertedReports()
+        {
+            List<Report> reports = await GetReports();
             var manHours = new ManHours();
-            
-            List<ReportViewModel> reportViewModelsList = reportList.Select(x => new ReportViewModel
+
+            List<ReportViewModel> vmreports = reports.Select(x => new ReportViewModel
             {
                 Description = x.Description,
                 ClientName = x.Client.Name,
-                WorkerName = x.Worker.FirstName + " " +x.Worker.LastName,
+                WorkerName = x.Worker.FirstName + " " + x.Worker.LastName,
                 StartDate = x.StartDate.Date.ToString("dd/MM/yyyy"),
-                ManHours = manHours.GetManHours(x.StartDate,x.EndDate),
+                ManHours = manHours.GetManHours(x.StartDate, x.EndDate),
                 Representant = x.Client.Representant,
                 ReportID = x.ID
             }).ToList();
-            
-            return View(reportViewModelsList);
+
+            return vmreports;
+        }
+
+        // GET
+        public async Task<IActionResult> Index()
+        {
+            return View(await ConvertedReports());
         }
 
         public async Task<IActionResult> Edit(int id)
         {
-            var data = await _context.Reports.FirstOrDefaultAsync(x => x.ID == id);
+            var data = await ConvertedReports();
 
-            return View();
+            return View(data.Where(x => x.ReportID == id).FirstOrDefault());
         }
+
         public IActionResult Details()
         {
             return View();
         }
+
         public IActionResult Delete()
         {
             return View();
