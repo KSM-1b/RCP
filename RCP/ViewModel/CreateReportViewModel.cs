2bfe3cd250bab922f8e62f0044b4b08e86b2855a
kajetanboruta <kajetanboruta@gmail.com>
2021-06-30 22:36:55 +0200

    added viewmodel - createreport

---
+++ RCP/ViewModel/CreateReportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using RCP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RCP.ViewModel
+{
+    public class CreateReportViewModel
+    {
+        public ReportViewModel ReportViewModel { get; set; }
+    }
+}
