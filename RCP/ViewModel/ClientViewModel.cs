a75a4bbf1af3eaae27c7064d829a4e52129fad9f
michal666-bit <gajewski.michal88@gmail.com>
2021-07-02 22:10:04 +0200

    Update ClientCreate and ClientViewModel

--- RCP/ViewModel/ClientViewModel.cs
+++ RCP/ViewModel/ClientViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RCP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace RCP.ViewModel
 {
     public class ClientViewModel
     {
+        public Client Client { get; set; }
     }
 }
