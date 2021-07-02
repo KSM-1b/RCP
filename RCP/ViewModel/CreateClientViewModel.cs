ae509349a4239bae4ed7c966d6f679dbbae5d328
michal666-bit <gajewski.michal88@gmail.com>
2021-07-02 21:22:10 +0200

    Update CreateClientViewModel

--- RCP/ViewModel/CreateClientViewModel.cs
+++ RCP/ViewModel/CreateClientViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using RCP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,6 @@ namespace RCP.ViewModel
 {
     public class CreateClientViewModel
     {
+        public ClientViewModel ClientViewModel { get; set; }
     }
 }
