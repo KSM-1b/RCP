66aee0ad170e0ec877e7e0a6a3ee296f0c36c401
michal666-bit <gajewski.michal88@gmail.com>
2021-07-02 21:11:27 +0200

    Update ClientController

--- RCP/Controllers/ClientController.cs
+++ RCP/Controllers/ClientController.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RCP.Models;
+using RCP.ViewModel;
 
 namespace RCP.Controllers
 {
-    public class ClientController
+    public class ClientController : Controller
     {
+        [HttpGet]
+        public async  Task<IActionResult> Create()
+        {
+            
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(CreateClientViewModel clientvm)
+        {
+
+        }
+
     }
 }
